487f7c0dae57621650348f751cafd6d2b425f12d
David Liedle <david.liedle@gmail.com>
2019-02-28 14:34:52 -0500

    Config cleanup

--- filestack/FilestackConfig.php
+++ filestack/FilestackConfig.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Filestack;
 
 /**
@@ -6,17 +7,17 @@ namespace Filestack;
  */
 class FilestackConfig
 {
-    const API_URL = 'https://www.filestackapi.com/api';
+    const API_URL     = 'https://www.filestackapi.com/api';
     const PROCESS_URL = 'https://process.filestackapi.com';
-    const CDN_URL = 'https://cdn.filestackcontent.com';
-    const UPLOAD_URL = 'https://upload.filestackapi.com';
+    const CDN_URL     = 'https://cdn.filestackcontent.com';
+    const UPLOAD_URL  = 'https://upload.filestackapi.com';
 
-    const UPLOAD_PART_SIZE = 1024 * 1024 * 8; // last_digit=MB
-    const UPLOAD_CHUNK_SIZE = 1024 * 1024 * 1; // last_digit=MB
-    const UPLOAD_MIN_CHUNK_SIZE = 1024 * 32; // last_digit=KB
+    const UPLOAD_PART_SIZE      = 1024 * 1024 * 8; // last_digit=MB
+    const UPLOAD_CHUNK_SIZE     = 1024 * 1024 * 1; // last_digit=MB
+    const UPLOAD_MIN_CHUNK_SIZE = 1024 * 32;       // last_digit=KB
 
-    const UPLOAD_WAIT_ATTEMPTS = 300;
-    const UPLOAD_WAIT_SECONDS = 2;
+    const UPLOAD_WAIT_ATTEMPTS   = 300;
+    const UPLOAD_WAIT_SECONDS    = 2;
     const UPLOAD_TIMEOUT_SECONDS = 30;
-    const MAX_RETRIES = 5;
+    const MAX_RETRIES            = 5;
 }
